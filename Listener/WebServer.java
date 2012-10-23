bdef3d7886642d02f5d04b9c426b96b9392c04b0
lacti <lactrious@gmail.com>
2012-10-23 23:14:43 +0900

    * add web-server frame (copy from netty example)

--- Listener/WebServer.java
+++ Listener/WebServer.java
@@ -1,5 +1,39 @@
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
 
 public class WebServer {
-	public static void main(String[] args) throws Exception {
-	}
+    private final int port;
+
+    public WebServer(int port) {
+        this.port = port;
+    }
+
+    public void run() {
+        // Configure the server.
+        ServerBootstrap bootstrap = new ServerBootstrap(
+                new NioServerSocketChannelFactory(
+                        Executors.newCachedThreadPool(),
+                        Executors.newCachedThreadPool()));
+
+        // Set up the event pipeline factory.
+        bootstrap.setPipelineFactory(new HttpSnoopServerPipelineFactory());
+
+        // Bind and start to accept incoming connections.
+        bootstrap.bind(new InetSocketAddress(port));
+    }
+
+    public static void main(String[] args) {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8080;
+        }
+        new WebServer(port).run();
+    }
+
 }
